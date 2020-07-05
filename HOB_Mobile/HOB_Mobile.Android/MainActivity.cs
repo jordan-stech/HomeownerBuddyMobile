9dfadd75a9842cf0c2b9ca2b0932b90f5407559f
Jacob McCallister <mccallister.54@buckeyemail.osu.edu>
2020-07-05 08:53:45 -0400

    Notifications Working and are schedulable

--- HOB_Mobile/HOB_Mobile.Android/MainActivity.cs
+++ HOB_Mobile/HOB_Mobile.Android/MainActivity.cs
@@ -3,12 +3,15 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.LocalNotification;
+using Android.Content;
 
 namespace HOB_Mobile.Droid
 {
     [Activity(Label = "HOB_Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
 
@@ -19,8 +22,20 @@ namespace HOB_Mobile.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            NotificationCenter.CreateNotificationChannel(new Plugin.LocalNotification.Platform.Droid.NotificationChannelRequest { });
+
             LoadApplication(new App());
+
+            NotificationCenter.NotifyNotificationTapped(Intent);
         }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            NotificationCenter.NotifyNotificationTapped(intent);
+            base.OnNewIntent(intent);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- HOB_Mobile/HOB_Mobile/App.xaml.cs
+++ HOB_Mobile/HOB_Mobile/App.xaml.cs
@@ -1,19 +1,30 @@
 ﻿using HOB_Mobile.Views;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using System.Threading.Tasks;
+using System;
+using Plugin.LocalNotification;
+using System.Collections.Generic;
 
 /* 
  * Main program which runs first on app startup
  */
 
+
+
 namespace HOB_Mobile
 {
     public partial class App : Application
     {
+
+
         public App()
         {
+
+
             // Component must be initialized
             InitializeComponent();
+
 
             //Check and see if user registered previously, if they have, redirect them to the HomePage
             //set the testing boolean to true to see register page for testing purposes
@@ -28,12 +39,16 @@ namespace HOB_Mobile
                 // Launch register page
                 MainPage = new NavigationPage(new Views.RegisterPage());
             }
+
+            NotificationCenter.Current.NotificationTapped += LoadPageFromNotification;
+
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
         }
+
 
         protected override void OnSleep()
         {
@@ -43,6 +58,28 @@ namespace HOB_Mobile
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        private void LoadPageFromNotification(NotificationTappedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.Data))
+            {
+                return;
+            }
+
+            var list = ObjectSerializer.DeserializeObject<List<string>>(e.Data);
+            if (list.Count != 1)
+            {
+                return;
+            }
+            if (list[0] != typeof(HOB_Mobile.Views.MaintenanceReminder).FullName)
+            {
+                return;
+            }
+            //var tapCount = list[1];
+
+            MainPage.Navigation.PushAsync((new HomePage(Preferences.Get("user_first_name", ""))));
+            MainPage.Navigation.PushAsync((new Views.MaintenanceReminder()));
         }
     }
 }
