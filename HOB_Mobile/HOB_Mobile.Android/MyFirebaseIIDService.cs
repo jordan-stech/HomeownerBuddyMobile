aec5f543bdc554c58758d5aa28b4ded4114b2fac
Jacob McCallister <mccallister.54@buckeyemail.osu.edu>
2020-07-13 14:39:01 -0400

    Firebase messaging implementation for android. Topic subscription for users to get notifications.

---
+++ HOB_Mobile/HOB_Mobile.Android/MyFirebaseIIDService.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+
+namespace HOB_Mobile.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+        void SendRegistrationToServer(string token)
+        {
+            // Add custom implementation, as needed.
+        }
+
+    }
+}
